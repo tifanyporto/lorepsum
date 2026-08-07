57b12f32d7f1b1f1e719037bceacdf308b3e3862
﻿Tifany Porto <168468858+tifanyporto@users.noreply.github.com>
2026-08-07 10:58:22 -0300

    .

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,3 +1,12 @@
 from sqlalchemy.orm import declarative_base
+from dotenv import load_dotenv
+import os
+from sqlalchemy import create_engine
 
 Base = declarative_base()
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
